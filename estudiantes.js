25c1e1223b05d3b2af76289ed3ff67d6bbc6b85c
Nicolas Massaccesi Perez <nicocarp1349@gmail.com>
2023-03-17 17:04:02 -0300

    Update estudiantes.js

--- estudiantes.js
+++ estudiantes.js
@@ -21,3 +21,45 @@ fetch('alumnos.json')
     });
   })
   .catch(error => console.error(error));
+
+
+  // Obtener la tabla donde se mostrarán los estudiantes
+const tablaEstudiantes = document.querySelector('#tabla-notas1 tbody');
+
+// Verificar si hay datos guardados en el localStorage
+const estudiantesGuardados = JSON.parse(localStorage.getItem('datos'));
+
+// Si hay datos guardados, agregarlos a la tabla
+if (estudiantesGuardados && estudiantesGuardados.length > 0) {
+  estudiantesGuardados.forEach((estudiante) => {
+    const fila = tablaEstudiantes.insertRow();
+    fila.innerHTML = `
+      <td>${estudiante.nombre}</td>
+      <td>${(estudiante.nota1 + estudiante.nota2) / 2}</td>
+      <td>${((estudiante.nota1 + estudiante.nota2) / 2) >= 7 ? 'Aprobado' : 'Reprobado'}</td>
+    `;
+  });
+}
+
+// Obtener el botón y el select del HTML
+const botonBuscar = document.querySelector('#boton-buscar');
+const selectEstado = document.querySelector('#select-estado');
+
+// Agregar event listener al botón buscar
+botonBuscar.addEventListener('click', () => {
+  // Obtener el valor seleccionado en el select
+  const estadoSeleccionado = selectEstado.value;
+
+  // Obtener todos los estudiantes de la tabla
+  const estudiantes = document.querySelectorAll('#tabla-notas tbody tr');
+
+  // Iterar sobre cada estudiante y ocultar/mostrar según el estado seleccionado
+  estudiantes.forEach((estudiante) => {
+    const estado = estudiante.querySelector('td:nth-child(3)').textContent.toLowerCase();
+    if (estadoSeleccionado === 'todos' || estadoSeleccionado === estado) {
+      estudiante.style.display = 'table-row';
+    } else {
+      estudiante.style.display = 'none';
+    }
+  });
+});
